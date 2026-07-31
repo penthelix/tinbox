37ecfdf58f720e0299ad67f21208516e9cc443e2
helix <commits.cider786@passfwd.com>
2026-08-01 01:57:31 +0530

    feat: add more feed URL management functions to storage module

--- src/storage.py
+++ src/storage.py
@@ -15,7 +15,15 @@ def set_config(config: dict) -> None:
         json.dump(config, f, indent=4)
 
 
-def set_attribute(key: str, value: str) -> None:
+def init_config() -> None:
+    set_config(
+        {
+            "feed_urls": [],
+        }
+    )
+
+
+def set_attribute(key: str, value: str | list) -> None:
     config = get_config()
     config[key] = value
     set_config(config)
@@ -28,3 +36,31 @@ def get_attribute(key: str) -> str:
 
 def get_feed_urls() -> set[str]:
     return set(get_attribute("feed_urls"))
+
+
+def set_feed_urls(urls: set[str]) -> None:
+    set_attribute("feed_urls", list(urls))
+
+
+def add_feed_url(url: str) -> None:
+    feed_urls: set[str] = get_feed_urls()
+    feed_urls.add(url)
+    set_feed_urls(feed_urls)
+
+
+def remove_feed_url(url: str) -> None:
+    feed_urls: set[str] = get_feed_urls()
+
+    if url in feed_urls:
+        feed_urls.remove(url)
+        set_feed_urls(feed_urls)
+    else:
+        raise URLNotFoundError(f"URL {url} not found in feed_urls")
+
+
+class URLNotFoundError(Exception):
+    """Exception raised when a URL is not found in config feed URLs"""
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
