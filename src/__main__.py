75c10f5159335ea63c9670d0f26e63312a8f19f7
helix <commits.cider786@passfwd.com>
2026-08-10 18:50:47 +0530

    refactor: rename feed flag to inbox
    Update all references from `feed` to `inbox`. Add empty state messages
    for inbox and list commands.

--- src/__main__.py
+++ src/__main__.py
@@ -7,7 +7,7 @@ from src import fetch
 def one_hot_vector(args: argparse.Namespace) -> list[bool]:
     # If you modify this list, make sure to update the if statement in parse() as well.
     return [
-        args.feed,
+        args.inbox,
         not args.add is None,
         not args.delete is None,
         args.list,
@@ -15,7 +15,7 @@ def one_hot_vector(args: argparse.Namespace) -> list[bool]:
 
 
 def validate_args(args) -> argparse.Namespace:
-    assert isinstance(args.feed, bool), f"args.feed is {type(args.feed)}"
+    assert isinstance(args.inbox, bool), f"args.inbox is {type(args.inbox)}"
     assert isinstance(args.add, str | None), f"args.add is {type(args.add)}"
     assert isinstance(args.delete, str | None), f"args.delete is {type(args.delete)}"
     assert isinstance(args.list, bool), f"args.list is {type(args.list)}"
@@ -28,15 +28,19 @@ def validate_args(args) -> argparse.Namespace:
         )
 
     if sum(is_arg_given) == 0:
-        args.feed = True
+        args.inbox = True
 
     return args
 
 
 def parse(args: argparse.Namespace):
-    def _feed():
+    def _inbox():
         videos = fetch.fetch_videos()
         videos = fetch.remove_shorts(videos)
+
+        if len(videos) == 0:
+            print("No videos in inbox. Run with -h for help.")
+            return
 
         print(f"Found {len(videos)} videos from {len(fetch.FEED_URLS)} feeds.")
         for video in videos:
@@ -59,6 +63,11 @@ def parse(args: argparse.Namespace):
     def _list():
         config = cfg.get_config()
         feeds = cfg.get_feed_urls(config)
+
+        if not feeds:
+            print("No feeds found. Run with -h for help.")
+            return
+
         print(f"Your feeds: {feeds}")
 
     vector = one_hot_vector(args)
@@ -68,7 +77,7 @@ def parse(args: argparse.Namespace):
 
     # If you modify this, make sure to update one_hot_vector() as well.
     if vector[0]:
-        _feed()
+        _inbox()
     elif vector[1]:
         _add()
     elif vector[2]:
@@ -83,9 +92,7 @@ if __name__ == "__main__":
         description="Your distraction-free YouTube inbox in the terminal",
         epilog="Get started by adding a feed with --add",
     )
-    parser.add_argument(
-        "-f", "--feed", action="store_true", help="get the feed you want to read"
-    )
+    parser.add_argument("-i", "--inbox", action="store_true", help="get your inbox")
     parser.add_argument("-a", "--add", action="store", help="add a feed")
     parser.add_argument("-d", "--delete", action="store", help="delete a feed")
     parser.add_argument("-l", "--list", action="store_true", help="list all feeds")
